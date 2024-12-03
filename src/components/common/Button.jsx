93eca6872304914498ae56562ef3518eed95253a
unknown <deepakradialcode@gmail.com>
2024-12-03 11:49:59 +0530

    slider creatr

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = ({text, svg, gap}) => {
   return (
     <div>
-      <button className={`px-9 py-3 bg-dark-red font-oswald font-bold text-lg leading-[27px] transition-all duration-300 hover:scale-90 ${gap}`}>{svg}{text}</button>
+      <button className={`px-9 py-3 bg-dark-red font-oswald font-bold text-lg leading-[27px] transition-all duration-300 hover:scale-90 flex items-center text-white rounded-lg text-nowrap max-lg:text-base max-lg:py-2 max-lg:px-7 max-sm:text-sm ${gap}`}>{svg}{text}</button>
     </div>
   )
 }
